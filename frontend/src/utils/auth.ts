ed671e52fc7ab310ad6cd3a65b5f13810eadb459
Debo <deemohuang@proton.me>
2025-02-04 19:34:56 -0500

    changed client

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -2,7 +2,7 @@ import { createAuth0 } from '@auth0/auth0-vue';
 
 export default createAuth0({
   domain: 'dev-cqv3m8q2efiz3xqi.us.auth0.com',
-  clientId: 'aWMsbueSlA2ikWinmqBmVm62OWhC1Mdy',
+  clientId: 'XncL2kFmjMpxhnRzEGMwV02KwslpTWFD',
   authorizationParams: {
     redirect_uri: window.location.origin,
     audience: 'https://portfolio-xd.com'
